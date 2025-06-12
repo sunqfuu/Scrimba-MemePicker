d63b1fa4e72ce0b1ead7330989be0df61b63109c
Orhan Sağır <sagir.o@outlook.com>
2025-06-12 18:08:01 +0300

    Implements meme modal display with cat image
    Updates renderCat function to display selected cat image in modal interface.
    Adds modal-related DOM elements and populates modal content with the selected
    cat image and alt text for accessibility.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { catsData } from '/data.js'
 const emotionRadios = document.getElementById('emotion-radios')
 const getImageBtn = document.getElementById('get-image-btn')
 const gifsOnlyOption = document.getElementById('gifs-only-option')
+const memeModal = document.getElementById('meme-modal')
+const memeModalInner = document.getElementById('meme-modal-inner')
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
@@ -45,7 +47,14 @@ function getSingleCatObject() {
 }
 
 function renderCat() {
-    getSingleCatObject() // temporary 
+    const catObject = getSingleCatObject()
+    memeModalInner.innerHTML = `
+        <img 
+        class="cat-img" 
+        src="./images/${catObject.image}"
+        alt="${catObject.alt}"
+        >`
+    memeModal.style.display = "flex"
 }
 
 function getEmotionsArray(cats) {
